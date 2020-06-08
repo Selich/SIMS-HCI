e76fcbdc198e002cc14b5d7f641ef74c4a32f16d
Dusan Urosevic <dusan0098@gmail.com>
2020-06-08 18:29:51 +0200

    Add Drag'nDrop-Director

--- Project/Views/Director/InventoryManagmentModal.xaml.cs
+++ Project/Views/Director/InventoryManagmentModal.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Project.Model;
+using Project.Views.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,15 +22,102 @@ namespace Project.Views.Director
     /// </summary>
     public partial class InventoryManagmentModal : Window
     {
+        Point startPoint = new Point();
+
+        public ObservableCollection<EquipmentDTO> Equipment
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<EquipmentDTO> Equipment2
+        {
+            get;
+            set;
+        }
+
         public InventoryManagmentModal()
         {
+            
             InitializeComponent();
-            List<Equipment> equipment = new List<Equipment>();
-            equipment.Add(new Equipment() { name="Sto", type = "namestaj" });
-            equipment.Add(new Equipment() { name = "Stolica", type = "namestaj" });
-            equipment.Add(new Equipment() { name="Cekic", type = "alat/oruzje"});
+            this.DataContext = this;
+            ObservableCollection<EquipmentDTO> l1 = new ObservableCollection<EquipmentDTO>();
+            l1.Add(new EquipmentDTO() { Name = "Sto", Type = "namestaj" });
+            l1.Add(new EquipmentDTO() { Name = "Stolica", Type = "namestaj" });
+            l1.Add(new EquipmentDTO() { Name="Cekic", Type = "alat/oruzje"});
 
-            DataGridExample.ItemsSource = equipment;
+            ObservableCollection<EquipmentDTO> l2 = new ObservableCollection<EquipmentDTO>();
+            l2.Add(new EquipmentDTO() { Name = "Makaze", Type = "alat" });
+            l2.Add(new EquipmentDTO() { Name = "Cekic", Type = "alat" });
+            l2.Add(new EquipmentDTO() { Name = "Cekic", Type = "alat/oruzje" });
+
+            Equipment = new ObservableCollection<EquipmentDTO>(l1);
+            Equipment2 = new ObservableCollection<EquipmentDTO>(l2);
+
+     
+        }
+
+        private void ListView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(null);
+        }
+
+        private void ListView_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startPoint - mousePos;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                // Get the dragged ListViewItem
+                DataGrid dgrid = sender as DataGrid;
+                DataGridRow item =
+                    FindAncestor<DataGridRow>((DependencyObject)e.OriginalSource);
+
+                if (item == null) return;
+
+                // Find the data behind the ListViewItem
+                EquipmentDTO equip = (EquipmentDTO)dgrid.ItemContainerGenerator.
+                    ItemFromContainer(item);
+
+                // Initialize the drag & drop operation
+                DataObject dragData = new DataObject("myFormat", equip);
+                DragDrop.DoDragDrop(item, dragData, DragDropEffects.Move);
+            }
+        }
+
+        private static T FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        {
+            do
+            {
+                if (current is T)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            }
+            while (current != null);
+            return null;
+        }
+
+        private void ListView_DragOver(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent("myFormat") || e.Source == sender)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void ListView_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("myFormat"))
+            {
+                EquipmentDTO equip = e.Data.GetData("myFormat") as EquipmentDTO;
+                Equipment.Remove(equip);
+                Equipment2.Add(equip);
+            }
         }
 
         private void CloseInventoryManagment(object sender, RoutedEventArgs e)
