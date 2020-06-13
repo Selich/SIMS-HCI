cf123777300b0d25cc447c0abadc13ee2df4ea69
Nikola Selic <selich.work@gmail.com>
2020-06-13 14:39:50 +0200

    Add Report Generators

---
+++ Project/Utility/GenerateDoctorReport.cs
@@ -0,0 +1,17 @@
+﻿using Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Utility
+{
+    class GenerateDoctorReport : IPDFReport
+    {
+        public Report GenerateReport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project/Utility/IPDFReport.cs
@@ -0,0 +1,16 @@
+﻿using Project.Model;
+using Project.Model.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Utility
+{
+    public interface IPDFReport
+    {
+        Report GenerateReport();
+
+    }
+}
