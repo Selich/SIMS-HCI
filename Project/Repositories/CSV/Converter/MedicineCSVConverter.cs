4d01d18a9a0d0ee0bb78653288e1cb893afa8460
Nikola Selic <selich.work@gmail.com>
2020-06-23 00:45:42 +0200

    Edit Medicine

--- Project/Repositories/CSV/Converter/MedicineCSVConverter.cs
+++ Project/Repositories/CSV/Converter/MedicineCSVConverter.cs
@@ -23,11 +23,16 @@ namespace Project.Repositories.CSV.Converter
         public Medicine ConvertCSVFormatToEntity(string medicineCSVFormat)
         {
             string[] tokens = medicineCSVFormat.Split(_delimiter.ToCharArray());
+        // public Medicine(long id, string purpose, string administration, bool approved, int quantity, string type, string description, string name)
             return new Medicine(
                 long.Parse(tokens[0]),
                 tokens[1],
                 tokens[2],
                 (long.Parse(tokens[3]) == 1)
+                int.Parse(tokens[4]),
+                tokens[5],
+                tokens[6],
+                tokens[7]
             );
         }
 
