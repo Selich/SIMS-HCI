8d0f00d4bf56c39b6e3fbe24fb590305989f8d98
Filip <filipzdelar@gmail.com>
2020-06-22 21:10:16 +0200

    Prescription Repository

---
+++ Project/Repositories/PrescriptionRepository.cs
@@ -0,0 +1,28 @@
+﻿using Project.Model;
+using Project.Repositories.Abstract;
+using Project.Repositories.CSV;
+using Project.Repositories.CSV.Stream;
+using Project.Repositories.Sequencer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project.Repositories
+{
+    public class PrescriptionRepository : CSVRepository<Prescription, long>, IPrescriptionRepository, IEagerCSVRepository<Prescription, long>
+    {
+        private const string ENTITY_NAME = "Prescription";
+
+        public PrescriptionRepository(
+            ICSVStream<Prescription> stream,
+            ISequencer<long> sequencer
+            ) : base(ENTITY_NAME, stream, sequencer)
+        {
+        }
+
+        public new IEnumerable<Prescription> Find(Func<Prescription, bool> predicate) => GetAllEager().Where(predicate);
+        
+        public IEnumerable<Prescription> GetAllEager() => GetAll();
+        public Prescription GetEager(long id) => GetById(id);
+    }
+}
