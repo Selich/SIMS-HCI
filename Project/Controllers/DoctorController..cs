d6d1aa5a51c8234c7a3fb674389b4e48039429b7
Filip <filipzdelar@gmail.com>
2020-06-23 23:10:49 +0200

    Doctor Constroller

---
+++ Project/Controllers/DoctorController..cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Project.Controllers;
+using Project.Views.Model;
+using Project.Model;
+using Project.Services;
+using Project.Views.Converters;
+
+namespace Project.Controllers
+{
+    /// <summary>
+    /// Interaction logic for DoctorController.xaml
+    /// </summary>
+    public partial class DoctorController : IController<DoctorDTO, long>
+    {
+        private IService<Doctor, long> _service;
+        private IConverter<Doctor, DoctorDTO> _converter;
+
+        public DoctorController(IService<Doctor, long> service, IConverter<Doctor, DoctorDTO> converter)
+        {
+            _service = service;
+            _converter = converter;
+        }
+
+
+        public DoctorDTO GetById(long id) => _converter.ConvertEntityToDTO(_service.GetById(id));
+
+        public IEnumerable<DoctorDTO> GetAll() => _converter.ConvertListEntityToListDTO((List<Doctor>)_service.GetAll());
+
+        public DoctorDTO Remove(DoctorDTO entity)
+        {
+            Doctor doctor = _service.Remove(_converter.ConvertDTOToEntity(entity));
+            return _converter.ConvertEntityToDTO(doctor);
+        }
+
+        public DoctorDTO Save(DoctorDTO entity)
+        {
+            Doctor doctor = _service.Save(_converter.ConvertDTOToEntity(entity));
+            return _converter.ConvertEntityToDTO(doctor);
+        }
+
+        public DoctorDTO Update(DoctorDTO entity)
+        {
+            Doctor doctor = _service.Update(_converter.ConvertDTOToEntity(entity));
+            return _converter.ConvertEntityToDTO(doctor);
+        }
+    }
+}
