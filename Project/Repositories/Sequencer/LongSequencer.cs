1e2d9ee11a312fcce68cbfb6dce1430665be20fc
Nikola Selic <selich.work@gmail.com>
2020-06-24 20:10:03 +0200

    Fix Sequencer

--- Project/Repositories/Sequencer/LongSequencer.cs
+++ Project/Repositories/Sequencer/LongSequencer.cs
@@ -4,7 +4,7 @@ namespace Project.Repositories.Sequencer
     {
         private long _nextId;
 
-        public long GenerateId() => _nextId++;
+        public long GenerateId() => ++_nextId;
 
         public void Initialize(long initId) => _nextId = initId;
     }
