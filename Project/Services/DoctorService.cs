58820e2fe0e7a3784cf2d928529b1d1fc62de2a5
Filip <filipzdelar@gmail.com>
2020-06-24 09:07:38 +0200

    Doctor Service Repository

---
+++ Project/Services/DoctorService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Project.Model;
+using Project.Repositories;
+using Project.Repositories.Abstract;
+
+namespace Project.Services
+{
+    class DoctorService : IService<Doctor, long>
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        private readonly AddressService _addressService;
+
+        public DoctorService(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        public IEnumerable<Doctor> GetAll() => _doctorRepository.GetAll();
+
+        public Doctor GetById(long id) => _doctorRepository.GetById(id);
+
+        public Doctor Save(Doctor patient)
+        {
+            patient.Address = _addressService.Save(patient.Address);
+            return _doctorRepository.Save(patient);
+        }
+
+        public Doctor Update(Doctor doctor)
+        {
+            doctor.Address = _addressService.Save(doctor.Address);
+            return _doctorRepository.Update(doctor);
+        }
+
+        public Doctor Remove(Doctor client) => _doctorRepository.Remove(client);
+
+    }
+}
