0e076bd1b0932f38a711ffe85728df175447761b
Filip <filipzdelar@gmail.com>
2020-06-24 16:55:44 +0200

    Hospitalless

--- Project/Views/Converters/DoctorConverter.cs
+++ Project/Views/Converters/DoctorConverter.cs
@@ -18,11 +18,11 @@ namespace Project.Views.Converters
         }
 
         public Project.Model.Doctor ConvertDTOToEntity(DoctorDTO dto)
-            => new Project.Model.Doctor(dto.Id, new Address(), dto.FirstName, dto.LastName, dto.Jmbg, dto.TelephoneNumber, dto.Gender, dto.DateOfBirth, dto.Salary, dto.AnnualLeave, dto.WorkingHours, dto.Email, dto.Password,  dto.MedicalRole, _hospitalCnverter.ConvertDTOToEntity(dto.Hospital));
+            => new Project.Model.Doctor(dto.Id, new Address(), dto.FirstName, dto.LastName, dto.Jmbg, dto.TelephoneNumber, dto.Gender, dto.DateOfBirth, dto.Salary, dto.AnnualLeave, dto.WorkingHours, dto.Email, dto.Password,  dto.MedicalRole);
 
 
         public DoctorDTO ConvertEntityToDTO(Project.Model.Doctor entity)
-            => new DoctorDTO(entity.Id, new AddressDTO(), entity.FirstName, entity.LastName, entity.Jmbg, entity.TelephoneNumber, entity.Gender, entity.DateOfBirth, entity.Salary, entity.AnnualLeave, entity.WorkingHours, entity.Email, entity.Password, _hospitalCnverter.ConvertEntityToDTO(entity.Hospital), entity.MedicalRole);
+            => new DoctorDTO(entity.Id, new AddressDTO(), entity.FirstName, entity.LastName, entity.Jmbg, entity.TelephoneNumber, entity.Gender, entity.DateOfBirth, entity.Salary, entity.AnnualLeave, entity.WorkingHours, entity.Email, entity.Password, entity.MedicalRole);
 
         public List<Project.Model.Doctor> ConvertListDTOToListEntity(IEnumerable<DoctorDTO> dtos)
             => dtos.Select(dto => ConvertDTOToEntity(dto)).ToList();
