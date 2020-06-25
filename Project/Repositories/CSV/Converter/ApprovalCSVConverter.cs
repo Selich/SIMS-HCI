5ea073926abe816616d7e57a0f2dbdb52267e9dc
Filip <filipzdelar@gmail.com>
2020-06-25 17:27:21 +0200

    comm

---
+++ Project/Repositories/CSV/Converter/ApprovalCSVConverter.cs
@@ -0,0 +1,32 @@
+﻿using Project.Model;
+
+namespace Project.Repositories.CSV.Converter
+{
+    public class ApprovalCSVConverter : ICSVConverter<Approval>
+    {
+        private readonly string _delimiter;
+
+        public ApprovalCSVConverter(string delimiter)
+        {
+            _delimiter = delimiter;
+        }
+
+        public string ConvertEntityToCSVFormat(Approval approval)
+          => string.Join(_delimiter,
+              approval.Description,
+              approval.IsApproved,
+              approval.Doctor,
+              approval.Proposition
+              );
+
+        public Approval ConvertCSVFormatToEntity(string approvalCSVFormat)
+        {
+            string[] tokens = approvalCSVFormat.Split(_delimiter.ToCharArray());
+            return new Approval(
+                long.Parse(tokens[0]),
+                tokens[1],
+                tokens[2],
+                tokens[3]);
+        }
+    }
+}
