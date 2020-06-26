a83b55075acddf3ec8c209b19c1d3e7d31375f93
Nikola Selic <selich.work@gmail.com>
2020-06-26 14:43:18 +0200

    Add Referral Repositories

---
+++ Project/Repositories/Referral/OperationReferralRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Repositories.Referral
+{
+    public class OperationReferralRepository : IReferralRepository
+    {
+        public IEnumerable<Model.Referral> Find(Func<Model.Referral, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.Referral> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.Referral GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.Referral Remove(Model.Referral entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.Referral Save(Model.Referral entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.Referral Update(Model.Referral entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
